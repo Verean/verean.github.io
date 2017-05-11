ef74548e7d372f3ba6747e1620db8160073803fa
Verean <verean@gmail.com>
2017-05-11 10:46:27 -0500

    PWA Fullscreen

--- app/service-worker.js
+++ app/service-worker.js
@@ -7,10 +7,10 @@ var filesToCache = [
 ];
 
 self.addEventListener('install', function(e) {
-	console.log('[PWA - ServiceWorker] Install event fired.');
+	console.log('[ServiceWorker] Install event fired.');
 	e.waitUntil(
 		caches.open(cacheName).then(function(cache) {
-			console.log('[PWA - ServiceWorker] Caching app shell...');
+			console.log('[ServiceWorker] Caching app shell...');
 			return cache.addAll(filesToCache).then(function() {
 				self.skipWaiting();
 			});
@@ -19,12 +19,12 @@ self.addEventListener('install', function(e) {
 });
 
 self.addEventListener('activate', function(e) {
-	console.log('[PWA - ServiceWorker] Activate event fired.');
+	console.log('[ServiceWorker] Activate event fired.');
 	e.waitUntil(
 		caches.keys().then(function(keyList) {
 			return Promise.all(keyList.map(function(key) {
 				if (key !== cacheName) {
-					console.log('[PWA - ServiceWorker] Removing old cache...', key);
+					console.log('[ServiceWorker] Removing old cache...', key);
 					return caches.delete(key);
 				}
 			}));
@@ -34,16 +34,16 @@ self.addEventListener('activate', function(e) {
 });
 
 self.addEventListener('fetch', function(e) {
-	console.log('[PWA - ServiceWorker] Fetch event fired.', e.request.url);
+	console.log('[ServiceWorker] Fetch event fired.', e.request.url);
 	e.respondWith(
 		caches.match(e.request).then(function(response) {
 			if (response) {
-				console.log('[PWA - ServiceWorker] Retrieving from cache...');
+				console.log('[ServiceWorker] Retrieving from cache...');
 				return response;
 			}
-			console.log('[PWA - ServiceWorker] Retrieving from URL...');
+			console.log('[ServiceWorker] Retrieving from URL...');
 			return fetch(e.request).catch(function(e){
-				console.log('[PWA - ServiceWorker] Fetch request failed!');
+				console.log('[ServiceWorker] Fetch request failed!');
 			});
 		})
 	);
