94aae58db53db0a89f41b81dfb3b23b2a50ce9f9
Verean <verean@gmail.com>
2017-10-15 20:00:43 -0600

    Proper formatting

--- giphy/assets/js/app.js
+++ giphy/assets/js/app.js
@@ -1,41 +1,42 @@
-var publicKey = 'dc6zaTOxFJmzC';
-var apiEndPoint = 'https://api.giphy.com/v1/gifs/search';
+var publicKey = 'dc6zaTOxFJmzC'
+var apiEndPoint = 'https://api.giphy.com/v1/gifs/search'
 
 new Vue({
-    el: '#app',
-    data: {
-        query: '',
-        results: false,
-        current_gif: false,
-        source: false,
-        rating: false,
-        date: false
-    },
-    methods: {
-        viewGIF: function (gif) {
-            this.current_gif = gif.images.original.url;
-            this.source = gif.source;
-            this.rating = gif.rating;
-            this.date = gif.import_datetime;
-            //console.log(gif);
-        }
-    },
-    computed: {
-        liveSearch: function () {
-            var self = this;
-            axios.get(apiEndPoint, {
-                params: {
-                    api_key: publicKey,
-                    q: self.query.split(' ').join('+'),
-                    limit: 20
-                }
-            })
-            .then(function (response) {
-                self.results = response.data.data;
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-        }
-  }
-});
+	el: '#app',
+	data: {
+		query: '',
+		results: false,
+		current_gif: false,
+		source: false,
+		rating: false,
+		date: false
+	},
+	methods: {
+		viewGIF: function(gif) {
+			this.current_gif = gif.images.original.url
+			this.source = gif.source
+			this.rating = gif.rating
+			this.date = gif.import_datetime
+			//console.log(gif);
+		}
+	},
+	computed: {
+		liveSearch: function() {
+			var self = this
+			axios
+				.get(apiEndPoint, {
+					params: {
+						api_key: publicKey,
+						q: self.query.split(' ').join('+'),
+						limit: 20
+					}
+				})
+				.then(function(response) {
+					self.results = response.data.data
+				})
+				.catch(function(error) {
+					console.log(error)
+				})
+		}
+	}
+})
